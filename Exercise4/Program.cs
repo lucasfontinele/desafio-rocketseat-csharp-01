69e46cd5c36a90a0cbc5cc15ee1bcb5d4af960b0
Lucas Fontinele <contato@fontinele.dev>
2024-03-13 03:02:27 -0300

    chore: changes

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<string> words = [];
+
+Console.WriteLine("Quantas palavras deseja inserir?");
+
+var wordsQuantity = Console.ReadLine();
+var quantity = int.Parse(wordsQuantity);
+
+for (var round = 0; round < quantity; round++)
+{
+    var current = round + 1;
+    
+    Console.WriteLine($"Digite a palavra {current}:");
+
+    var word = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(word))
+    {
+        return;
+    }
+    
+    words.Add(word.Trim().Replace(" ", ""));
+}
+
+words.ForEach(word =>
+{
+    Console.WriteLine($"A palavra {word} tem {word.Length} caracteres.");
+});
---
+++ Exercise5/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Informe a placa do veículo:");
+
+var carPlateEntry = Console.ReadLine()?.Trim();
+
+bool IsValidPlate()
+{
+    if (string.IsNullOrEmpty(carPlateEntry) || string.IsNullOrWhiteSpace(carPlateEntry) || carPlateEntry.Length != 7)
+    {
+        return false;
+    }
+    
+    for (var i = 0; i < 3; i++)
+    {
+        var current = carPlateEntry[i];
+
+        if (!char.IsLetter(current))
+        {
+            return false;
+        }
+    }
+
+    for (var i = 3; i < 7; i++)
+    {
+        var current = carPlateEntry[i];
+
+        if (!char.IsDigit(current))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+Console.WriteLine(IsValidPlate());
+
+
