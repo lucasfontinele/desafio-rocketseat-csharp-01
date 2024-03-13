060b942097170f88c57f5bb7c4fd44c1bc5fde06
Lucas Fontinele <contato@fontinele.dev>
2024-03-13 03:12:52 -0300

    chore: finishes challenge

---
+++ Exercise6/Program.cs
@@ -0,0 +1,11 @@
+﻿var now = DateTime.UtcNow;
+
+var longDate = $"{now.DayOfWeek}, {now.Day}, {now.Month}, {now.Year}, {now.Hour}, {now.Minute}, {now.Second}";
+var defaultDate = now.ToShortDateString();
+var shortTime = now.ToShortTimeString();
+var month = now.ToString("dd 'de' MMMM 'de' yyyy");
+
+Console.WriteLine(longDate);
+Console.WriteLine(defaultDate);
+Console.WriteLine(shortTime);
+Console.WriteLine(month);
